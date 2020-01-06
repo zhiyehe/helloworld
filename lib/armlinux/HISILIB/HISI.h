249d6c1b001007a2dc3d6ec5cad0cea3d647f50f
zhihe <zhiye.he@nokia-sbell.com>
2020-01-06 13:51:34 +0800

    add lib for hisi

---
+++ lib/armlinux/HISILIB/HISI.h
@@ -0,0 +1,11 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int SAMPLE_VIO_Rotate(unsigned int u32VoIntfType);
+extern int SAMPLE_VIO_ViDoublePipeRouteToGetVPSS(unsigned int u32VoIntfType);
+//extern void initfaceprocess(void);
+extern void callback(unsigned char* rgb_ptr, int size);
+#ifdef __cplusplus
+}
+#endif
